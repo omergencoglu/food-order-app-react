d5fe731c13b5be26566652398df1877f66b73d1d
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-01-21 14:25:59 -0500

    Add loading state

---
+++ README.md
@@ -0,0 +1,8 @@
+# Food Ordering App with React
+
+Learning React in progress..
+
+This is a project to practice React.
+
+- Add items to your cart and cart will update automatically
+- Fetching meals data from Firebase database
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,6 +7,7 @@ import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchMeals = async () => {
@@ -25,10 +26,18 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
-
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   const mealList = meals.map((meal) => (
     <MealItem
